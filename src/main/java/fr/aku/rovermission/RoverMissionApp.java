7b16bd4fb2408148a49212ec6de0bd9f5c9dc7ed
Ajtene Kurtaliqi <ajtene.kurtaliqi@gmail.com>
2026-06-17 15:23:39 +0200

    add hello world basic template app

---
+++ src/main/java/fr/aku/rovermission/RoverMissionApp.java
@@ -0,0 +1,13 @@
+package fr.aku.rovermission;
+
+/**
+ * Hello world!
+ *
+ */
+public class RoverMissionApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/fr/aku/rovermission/RoverMissionAppTest.java
@@ -0,0 +1,13 @@
+package fr.aku.rovermission;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class RoverMissionAppTest {
+
+    @Test
+    void testRoverMissionApp() {
+        assertTrue(true);
+    }
+}
