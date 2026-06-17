2a869692ad3ea443493723d1fd6175ceb25b8a09
Ajtene Kurtaliqi <ajtene.kurtaliqi@gmail.com>
2026-06-17 19:02:01 +0200

    add test for rover

--- src/test/java/fr/aku/rovermission/domain/RoverTest.java
+++ src/test/java/fr/aku/rovermission/domain/RoverTest.java
@@ -1,18 +1,25 @@
 package fr.aku.rovermission.domain;
 
+import fr.aku.rovermission.application.MissionRunner;
+import fr.aku.rovermission.infrastructure.input.CommandParser;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RoverTest {
 
+    private final CommandParser commandParser = new CommandParser();
+    private final MissionRunner missionRunner = new MissionRunner();
+
     @Test
-    void sequenceLmlmlmlmmFromOneTwoNorthEndsAtOneThreeNorth() {
-        Rover rover = new Rover(new Position(1, 2), Direction.NORTH, new Plateau(5, 5));
+    void runMissionFromOneTwoNorthEndsAtOneThreeNorth() {
+        Rover rover = new Rover(new Position(1, 2), Direction.NORTH);
+        Mission mission = new Mission(rover, commandParser.parse("LMLMLMLMM"));
+        Plateau plateau = new Plateau(5, 5);
 
-        rover.execute("LMLMLMLMM");
+        missionRunner.run(mission, plateau);
 
-        assertEquals(new Position(1, 3), rover.getPosition());
-        assertEquals(Direction.NORTH, rover.getDirection());
+        assertEquals(new Position(1, 3), rover.position());
+        assertEquals(Direction.NORTH, rover.direction());
     }
 }
